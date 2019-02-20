f48734e68f4fd7c9c9a6df60a44ddc2ed31de6e6
Fredrik Kemling <kemling@gmail.com>
2019-02-20 19:55:52 +0100

    Added some unit tests

--- src/test/java/evolver/TestUtils.java
+++ src/test/java/evolver/TestUtils.java
@@ -5,10 +5,20 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class TestUtils {
+class TestUtils {
 
 
-    static void skapaBildMedFarg(int width, int height, int r, int g, int b, String pathToNewFile) {
+    /**
+     * Creates a PNG image file given dimensions and color in RGB
+     *
+     * @param width         width of image
+     * @param height        height of image
+     * @param r             red (0-255)
+     * @param g             green (0-255)
+     * @param b             blue (0-255)
+     * @param pathToNewFile the path to the new file including file name.
+     */
+    static void createPngFile(int width, int height, int r, int g, int b, String pathToNewFile) {
         BufferedImage newImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
         for (int x = 0; x < width; x++)
@@ -20,18 +30,39 @@ public class TestUtils {
             }
 
         System.out.println("Sparar diffad bild till: " + pathToNewFile);
-        try{
+        try {
             ImageIO.write(newImage, "png", new File(pathToNewFile));
-        }catch(IOException e){
-            System.out.println("Kunde inte spara bildfilen till: " +pathToNewFile);
+        } catch (IOException e) {
+            System.out.println("Kunde inte spara bildfilen till: " + pathToNewFile);
             e.printStackTrace();
         }
 
 
     }
 
-    public static void main(String[] args) {
-        skapaBildMedFarg(10,20,255,0,0,"red.png");
+    /**
+     * Creates a BufferedImage given dimensions and color in RGB
+     *
+     * @param width  width of image
+     * @param height height of image
+     * @param r      red (0-255)
+     * @param g      green (0-255)
+     * @param b      blue (0-255)
+     * @return a BufferedImage
+     */
+    static BufferedImage createBufferedImage(int width, int height, int r, int g, int b) {
+        BufferedImage newBufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++) {
+
+
+                int newColor = (255 << 24) | (r << 16) | (g << 8) | b;
+                newBufferedImage.setRGB(x, y, newColor);
+            }
+
+        return newBufferedImage;
     }
+
 
 }
---
+++ src/test/java/evolver/UtilsTest.java
@@ -0,0 +1,48 @@
+package evolver;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilsTest {
+
+    @RepeatedTest(20)
+    @DisplayName("Check that numbers are within interval")
+    void mutateInWholeIntervalTest1() {
+        int minValue = 3;
+        int maxValue = 3;
+        int returnedValue = Utils.mutateInWholeInterval(minValue, maxValue);
+        assertTrue(minValue <= returnedValue);
+        assertTrue(maxValue >= returnedValue);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0",
+            "10"
+    })
+    @DisplayName("Check that numbers returned are within given tight interval")
+    void mutateInWholeIntervalTest2(int sameValue) {
+        int returnedValue = Utils.mutateInWholeInterval(sameValue, sameValue);
+        assertTrue(sameValue <= returnedValue);
+        assertTrue(sameValue >= returnedValue);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Check that IllegalArgumentException is thrown.")
+    @CsvSource({
+            "-1,10",
+            "10,-1",
+            "-1,-2"
+    })
+    void throwsExceptionWhenBadArguments(int minValue, int maxValue) {
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+                Utils.mutateInWholeInterval(minValue, maxValue));
+        assertEquals("Parameters must be a positive number or zero.", exception.getMessage());
+    }
+
+
+}
