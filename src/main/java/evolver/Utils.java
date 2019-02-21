625ee261885e637b7422e835baebea5e152a63b1
Fredrik Kemling <kemling@gmail.com>
2019-02-21 12:50:58 +0100

    Added new method to generate random ints in given interval

--- src/main/java/evolver/Utils.java
+++ src/main/java/evolver/Utils.java
@@ -4,35 +4,85 @@ import java.util.Random;
 
 public class Utils {
 
-    //TODO: skriv unit-tester för denna
-    public static int mutateInInterval(int minInclusive, int maxInclusive, int currentVal, int maxStep) {
-        int returnVal = currentVal;
-        Random rand = new Random();
-        do {
-            int delta = 0;
-            do {
-                delta = rand.nextInt(maxStep * 2 + 1) - maxStep;
-            } while (delta == 0);
-            returnVal = currentVal + delta;
-        } while (returnVal < minInclusive || returnVal > maxInclusive);
-        return returnVal;
+  /**
+   * Returns a random int within the given interval (minIncl - maxIncl) A spread ratio close to 1.0
+   * will let the new random int generate within the whole interval. A spread ratio close to 0.01
+   * will have the new random int generate close to the current value. This method will not return a
+   * value equal to the currentVal
+   *
+   * @param minIncl smallest tolerated value to be returned
+   * @param maxIncl largest tolerated value to be returned
+   * @param currentVal the current value.
+   * @param spreadRatio The spread ratio (between 0.01 and 1.0).
+   * @return a new random number
+   */
+  public static int mutateInInterval(int minIncl, int maxIncl, int currentVal, float spreadRatio) {
+    if (spreadRatio < 0.01f || spreadRatio > 1) {
+      throw new IllegalArgumentException(
+          "maxStepPerc must be larger than 0.01 and not larger than 1.");
+    }
+    if (minIncl == maxIncl) {
+      throw new IllegalArgumentException("minIncl cannot be equal to maxIncl.");
+    }
+    if (minIncl > maxIncl) {
+      throw new IllegalArgumentException("minIncl cannot be greater than maxIncl.");
+    }
+    if (minIncl < 0 || maxIncl < 0 || currentVal < 0) {
+      throw new IllegalArgumentException("Parameters must be a positive number or zero.");
+    }
+    if (currentVal < minIncl || currentVal > maxIncl) {
+      throw new IllegalArgumentException("currentVal must be between minIncl and maxIncl");
     }
 
-
-    /**
-     * Returns a random in the given interval (including max/min value)
-     *
-     * @param minInclusive minimum value
-     * @param maxInclusive maximum value
-     * @return a random int int the given interval
-     */
-    static int mutateInWholeInterval(int minInclusive, int maxInclusive) {
-        if (minInclusive < 0 || maxInclusive < 0) {
-            throw new IllegalArgumentException("Parameters must be a positive number or zero.");
-        }
-        Random rand = new Random();
-        return (rand.nextInt(maxInclusive - minInclusive + 1) + minInclusive);
+    int intervalWidth = maxIncl - minIncl;
+    int newMaxIncl = (int) (currentVal + intervalWidth * spreadRatio);
+    if (newMaxIncl > maxIncl) {
+      newMaxIncl = maxIncl;
     }
+    int newMinIncl = (int) (currentVal - intervalWidth * spreadRatio);
+    if (newMinIncl < minIncl) {
+      newMinIncl = minIncl;
+    }
+    int newIntervalWidth = newMaxIncl - newMinIncl;
+
+    int returnVal = currentVal;
+    Random rand = new Random();
+    do {
+      returnVal = rand.nextInt(newIntervalWidth + 1) + newMinIncl;
+    } while (returnVal == currentVal);
+
+    return returnVal;
+  }
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 40; i++) {
+      int randomInt = mutateInInterval(0, 20, 15, 0.5f);
+      System.out.print(randomInt + "  ");
+    }
+  }
+
+
+  /**
+   * Returns a random int in the given interval (including max/min value).
+   *
+   * @param minInclusive minimum value
+   * @param maxInclusive maximum value
+   * @return a random int int the given interval
+   */
+  static int mutateInWholeInterval(int minInclusive, int maxInclusive) {
+    if (minInclusive < 0 || maxInclusive < 0) {
+      throw new IllegalArgumentException("Parameters must be a positive number or zero.");
+    }
+    if (minInclusive == maxInclusive) {
+      throw new IllegalArgumentException("minInclusive cannot be equal to maxInclusive.");
+    }
+    if (minInclusive > maxInclusive) {
+      throw new IllegalArgumentException("minInclusive cannot be greater than maxInclusive.");
+    }
+
+    Random rand = new Random();
+    return (rand.nextInt(maxInclusive - minInclusive + 1) + minInclusive);
+  }
 
 
 }
--- src/test/java/evolver/UtilsTest.java
+++ src/test/java/evolver/UtilsTest.java
@@ -1,48 +1,132 @@
 package evolver;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UtilsTest {
 
-    @RepeatedTest(20)
-    @DisplayName("Check that numbers are within interval")
-    void mutateInWholeIntervalTest1() {
-        int minValue = 3;
-        int maxValue = 8;
-        int returnedValue = Utils.mutateInWholeInterval(minValue, maxValue);
-        assertTrue(minValue <= returnedValue);
-        assertTrue(maxValue >= returnedValue);
+
+  @ParameterizedTest
+  @DisplayName("Check that IllegalArgumentException is thrown.")
+  @CsvSource({
+      "-1,10,3",
+      "-3,-1,-2"
+  })
+  void throwsExceptionWhenBadArguments1(int minValue, int maxValue, int current) {
+    Exception exception = assertThrows(IllegalArgumentException.class, () ->
+        Utils.mutateInInterval(minValue, maxValue, current, 0.5f));
+    assertEquals("Parameters must be a positive number or zero.", exception.getMessage());
+  }
+
+  @Test
+  @DisplayName("min value equals max value")
+  void throwsExceptionMaxEqualsMin1() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () ->
+        Utils.mutateInInterval(3, 3, 3, 0.5f));
+    assertEquals("minIncl cannot be equal to maxIncl.", exception.getMessage());
+  }
+
+  @Test
+  @DisplayName("min value equals max value")
+  void throwsExceptionMaxLessThanMin1() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () ->
+        Utils.mutateInInterval(5, 3, 4, 0.5f));
+    assertEquals("minIncl cannot be greater than maxIncl.", exception.getMessage());
+  }
+
+  @Test
+  @DisplayName("min value equals max value")
+  void throwsExceptionSpreadRatioTooSmall() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () ->
+        Utils.mutateInInterval(5, 3, 4, 0f));
+    assertEquals("maxStepPerc must be larger than 0.01 and not larger than 1.",
+        exception.getMessage());
+  }
+
+  @Test
+  @DisplayName("min value equals max value")
+  void throwsExceptionSpreadRatioTooLarge() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () ->
+        Utils.mutateInInterval(5, 3, 4, 1.001f));
+    assertEquals("maxStepPerc must be larger than 0.01 and not larger than 1.",
+        exception.getMessage());
+  }
+
+
+  @RepeatedTest(20)
+  @DisplayName("Check that random numbers are within interval. Not equal current value.")
+  void mutateInIntervalTest1() {
+    int minValue = 0;
+    int maxValue = 20;
+    int currentValue = 10;
+    float intervalWidthPerc = 0.2f;
+
+    int wholeInterval = maxValue - minValue;
+    int maxExpectedValue = (int) (currentValue + wholeInterval * intervalWidthPerc);
+    if (maxExpectedValue > maxValue) {
+      maxExpectedValue = maxValue;
+    }
+    int minExpectedValue = (int) (currentValue - wholeInterval * intervalWidthPerc);
+    if (minExpectedValue < minValue) {
+      minExpectedValue = minValue;
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "0",
-            "10"
-    })
-    @DisplayName("Check that numbers returned are within given tight interval")
-    void mutateInWholeIntervalTest2(int sameValue) {
-        int returnedValue = Utils.mutateInWholeInterval(sameValue, sameValue);
-        assertTrue(sameValue <= returnedValue);
-        assertTrue(sameValue >= returnedValue);
-    }
+    int returnedValue = Utils.mutateInInterval(minValue, maxValue, currentValue, intervalWidthPerc);
+    assertNotEquals(returnedValue, currentValue, "Returned value equals current value.");
+    assertTrue(returnedValue <= maxExpectedValue,
+        "Returned value too large (" + returnedValue + ").");
+    assertTrue(returnedValue >= minExpectedValue,
+        "Returned value too small (" + returnedValue + ")");
+  }
 
-    @ParameterizedTest
-    @DisplayName("Check that IllegalArgumentException is thrown.")
-    @CsvSource({
-            "-1,10",
-            "10,-1",
-            "-1,-2"
-    })
-    void throwsExceptionWhenBadArguments(int minValue, int maxValue) {
-        Exception exception = assertThrows(IllegalArgumentException.class, () ->
-                Utils.mutateInWholeInterval(minValue, maxValue));
-        assertEquals("Parameters must be a positive number or zero.", exception.getMessage());
-    }
+
+  @RepeatedTest(20)
+  @DisplayName("Check that random numbers are within interval")
+  void mutateInWholeIntervalTest1() {
+    int minValue = 3;
+    int maxValue = 8;
+    int returnedValue = Utils.mutateInWholeInterval(minValue, maxValue);
+    assertTrue(minValue <= returnedValue);
+    assertTrue(maxValue >= returnedValue);
+  }
+
+
+  @ParameterizedTest
+  @DisplayName("Check that IllegalArgumentException is thrown.")
+  @CsvSource({
+      "-1,10",
+      "10,-1",
+      "-1,-2"
+  })
+  void throwsExceptionWhenBadArguments(int minValue, int maxValue) {
+    Exception exception = assertThrows(IllegalArgumentException.class, () ->
+        Utils.mutateInWholeInterval(minValue, maxValue));
+    assertEquals("Parameters must be a positive number or zero.", exception.getMessage());
+  }
+
+  @Test
+  @DisplayName("min value equals max value")
+  void throwsExceptionMaxEqualsMin() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () ->
+        Utils.mutateInWholeInterval(3, 3));
+    assertEquals("minInclusive cannot be equal to maxInclusive.", exception.getMessage());
+  }
+
+  @Test
+  @DisplayName("min value equals max value")
+  void throwsExceptionMaxLessThanMin() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () ->
+        Utils.mutateInWholeInterval(4, 3));
+    assertEquals("minInclusive cannot be greater than maxInclusive.", exception.getMessage());
+  }
 
 
 }
