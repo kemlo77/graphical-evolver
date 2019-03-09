027ee8279cb11e6ee547269dc496b8cfb1626fd7
Fredrik Kemling <kemling@gmail.com>
2019-03-09 13:51:26 +0100

    minor changes

--- src/main/java/evolver/TargetImage.java
+++ src/main/java/evolver/TargetImage.java
@@ -1,38 +1,38 @@
 package evolver;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import javax.imageio.ImageIO;
 
 class TargetImage {
 
-    private int imageWidth;
-    private int imageHeight;
-    private int imageType;
-    private BufferedImage bufferedImage;
+  private int imageWidth;
+  private int imageHeight;
+  private int imageType;
+  private BufferedImage bufferedImage;
 
 
-    int getImageWidth() {
-        return imageWidth;
-    }
+  int getImageWidth() {
+    return imageWidth;
+  }
 
-    int getImageHeight() {
-        return imageHeight;
-    }
+  int getImageHeight() {
+    return imageHeight;
+  }
 
-    int getImageType() {
-        return imageType;
-    }
+  int getImageType() {
+    return imageType;
+  }
 
-    BufferedImage getBufferedImage() {
-        return bufferedImage;
-    }
+  BufferedImage getBufferedImage() {
+    return bufferedImage;
+  }
 
-    TargetImage(File file) throws IOException {
-        bufferedImage = ImageIO.read(file);
-        imageHeight = bufferedImage.getHeight();
-        imageWidth = bufferedImage.getWidth();
-        imageType = bufferedImage.getType();
-    }
+  TargetImage(File file) throws IOException {
+    bufferedImage = ImageIO.read(file);
+    imageHeight = bufferedImage.getHeight();
+    imageWidth = bufferedImage.getWidth();
+    imageType = bufferedImage.getType();
+  }
 }
