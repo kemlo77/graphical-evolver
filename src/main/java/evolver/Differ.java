1fbf0ad530fe9e68b9b2eaf25afbe73354df8267
Fredrik Kemling <kemling@gmail.com>
2020-03-19 23:28:11 +0100

    Alternativ metod för attt skapa diff-bild (createDeltaImage2)

--- src/main/java/evolver/Differ.java
+++ src/main/java/evolver/Differ.java
@@ -9,17 +9,14 @@ import javax.imageio.ImageIO;
 class Differ {
 
   public static void main(String[] args) throws IOException {
-
-    BufferedImage in1 = ImageIO.read(new File("mona_small.png"));
-    BufferedImage in2 = ImageIO.read(new File("result_95.96_1f.png"));
-    createDeltaImage(in1, in2, "diff.png");
+    BufferedImage in1 = ImageIO.read(new File("160px-Mona_Lisa.PNG"));
+    BufferedImage in2 = ImageIO.read(new File("result_96.37_1a.png"));
+    createDeltaImage2(in1, in2, "diff4.png");
   }
 
-
-
-
   static long totalImageColorDifference(BufferedImage image1, BufferedImage image2) {
-    //inspiration från https://stackoverflow.com/questions/37598977/how-to-compare-two-images-using-selenium
+    //inspiration från
+    // https://stackoverflow.com/questions/37598977/how-to-compare-two-images-using-selenium
     //se denna https://stackoverflow.com/questions/6524196/java-get-pixel-array-from-image
     //TODO: testa att det fungerar med bild utan alpha-channel
     //TODO: hantera om ena bilden har alpha-channel, men inte den andra?
@@ -55,7 +52,7 @@ class Differ {
     return ((DataBufferByte) image1.getRaster().getDataBuffer()).getData();
   }
 
-  private static int colorChannelDifference(byte value1, byte value2) {
+  private static int colorChannelDifference(int value1, int value2) {
     return Math.abs((value1 & 0xff) - (value2 & 0xff));
   }
 
@@ -63,7 +60,8 @@ class Differ {
       throws IOException {
     BufferedImage newImage = new BufferedImage(image1.getWidth(), image1.getHeight(),
         image1.getType());
-    //inspiration från https://stackoverflow.com/questions/37598977/how-to-compare-two-images-using-selenium
+    //inspiration från
+    // https://stackoverflow.com/questions/37598977/how-to-compare-two-images-using-selenium
     //TODO: skriva om så att den använder en effektivare java-kod
     //TODO: hantera om bilderna är olika stora
     //TODO: pathToNewFile borde kanske vara en Path eller File
@@ -76,67 +74,77 @@ class Differ {
         newImage.setRGB(column, row, imageDiff);
       }
     }
-
     ImageIO.write(newImage, "png", new File(pathToNewFile));
   }
+
 
   static void createDeltaImage2(BufferedImage image1, BufferedImage image2, String pathToNewFile)
       throws IOException {
     int width = image1.getWidth();
     int height = image1.getHeight();
     BufferedImage newImage = new BufferedImage(width, height, image1.getType());
-
+    int maxDiff = 1;
     for (int column = 0; column < width; column++) {
       for (int row = 0; row < height; row++) {
-        int colorValue1 = image1.getRGB(column, row);
-        int colorValue2 = image2.getRGB(column, row);
-        int imageDiff = getColorDifference(colorValue1, colorValue2);
-        newImage.setRGB(column, row, imageDiff);
+        int color1 = image1.getRGB(column, row);
+        int color2 = image2.getRGB(column, row);
+        int absoluteDiff = getAbsoluteColorDifference(color1, color2);
+        if (absoluteDiff > maxDiff) {
+          maxDiff = absoluteDiff;
+        }
       }
     }
-
+    //TODO: inte beräkna absoluteDiff två gånger
+    for (int column = 0; column < width; column++) {
+      for (int row = 0; row < height; row++) {
+        int color1 = image1.getRGB(column, row);
+        int color2 = image2.getRGB(column, row);
+        int absoluteDiff = getAbsoluteColorDifference(color1, color2);
+        int newAlpha = (int) ((double) absoluteDiff / maxDiff * 255);
+        int newColorAndAlpha = rgbBytesToColorInt(getRed(color1), getGreen(color1), getBlue(color1),
+            newAlpha);
+        newImage.setRGB(column, row, newColorAndAlpha);
+      }
+    }
     ImageIO.write(newImage, "png", new File(pathToNewFile));
   }
 
-  private static int getAbsoluteColorDifference(int colorInt1, int colorInt2) {
-    int redDiff = colorChannelDifference(getRedByte(colorInt1), getRedByte(colorInt2));
-    int greenDiff = colorChannelDifference(getGreenByte(colorInt1), getGreenByte(colorInt2));
-    int blueDiff = colorChannelDifference(getBlueByte(colorInt1), getBlueByte(colorInt2));
-    int absoluteDiff = (int) Math
-        .floor(Math.sqrt(Math.pow(redDiff, 2) + Math.pow(greenDiff, 2) + Math.pow(blueDiff, 2)));
-    return rgbBytesToColorInt(redDiff, greenDiff, blueDiff);
+  private static int rgbBytesToColorInt(int red, int green, int blue, int alpha) {
+    return (alpha << 24) | (red << 16) | (green << 8) | blue;
+  }
+
+  private static int getAbsoluteColorDifference(int colorValue1, int colorValue2) {
+    int redDiff = colorChannelDifference(getRed(colorValue1), getRed(colorValue2));
+    int greenDiff = colorChannelDifference(getGreen(colorValue1), getGreen(colorValue2));
+    int blueDiff = colorChannelDifference(getBlue(colorValue1), getBlue(colorValue2));
+    return (int) Math
+        .floor(Math.sqrt(
+            (Math.pow(redDiff, 2) + Math.pow(greenDiff, 2) + Math.pow(blueDiff, 2)) / 3));
   }
 
   private static int getColorDifference(int colorInt1, int colorInt2) {
-    int redDiff = colorChannelDifference(getRedByte(colorInt1), getRedByte(colorInt2));
-    int greenDiff = colorChannelDifference(getGreenByte(colorInt1), getGreenByte(colorInt2));
-    int blueDiff = colorChannelDifference(getBlueByte(colorInt1), getBlueByte(colorInt2));
-    return rgbBytesToColorInt(redDiff, greenDiff, blueDiff);
+    int redDiff = colorChannelDifference(getRed(colorInt1), getRed(colorInt2));
+    int greenDiff = colorChannelDifference(getGreen(colorInt1), getGreen(colorInt2));
+    int blueDiff = colorChannelDifference(getBlue(colorInt1), getBlue(colorInt2));
+    return rgbBytesToInvertedColorInt(redDiff, greenDiff, blueDiff, 255);
   }
 
-  private static int rgbBytesToColorInt(int redDiff, int greenDiff, int blueDiff) {
+  private static int rgbBytesToInvertedColorInt(int red, int green, int blue, int alpha) {
     //TODO: ska det verkligen inverteras??
-    return (255 << 24) | ~((redDiff << 16) | (greenDiff << 8) | blueDiff);
+    return (alpha << 24) | ~((red << 16) | (green << 8) | blue);
   }
 
-  private static byte getRedByte(int colorInt) {
-    return (byte) ((colorInt >> 16) & 0xFF);
+  private static int getRed(int colorInt) {
+    return ((colorInt >> 16) & 0xFF);
   }
 
-  private static byte getGreenByte(int colorInt) {
-    return (byte) ((colorInt >> 8) & 0xFF);
+  private static int getGreen(int colorInt) {
+    return ((colorInt >> 8) & 0xFF);
   }
 
-  private static byte getBlueByte(int colorInt) {
-    return (byte) (colorInt & 0xFF);
+  private static int getBlue(int colorInt) {
+    return (colorInt & 0xFF);
   }
-
-
-
-
-
-
-
 
 
 }
